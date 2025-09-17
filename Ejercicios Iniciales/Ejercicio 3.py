e29c1e3a5ebfd513960c1327b205c5bc5a8ae5ea
Pau-Rodriguez-Gago <pau.rodriguez@campus.xaloc.org>
2025-09-17 09:56:12 +0200

    Create Ejercicio 3.py

---
+++ Ejercicios Iniciales/Ejercicio 3.py
@@ -0,0 +1,5 @@
+#3.-Programa que pida dos números enteros y realice la suma correspondiente
+var1= int(input("Introduce un número entero: "))
+var2= int(input("Introduce otro número entero: "))
+resultado= var1 + var2
+print("La suma de los dos números enteros es: ",resultado)

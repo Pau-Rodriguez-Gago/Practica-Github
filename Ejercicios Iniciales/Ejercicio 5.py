f343047542beaf09fdaaabdb90443c61132a5eba
Pau-Rodriguez-Gago <pau.rodriguez@campus.xaloc.org>
2025-09-17 09:56:18 +0200

    Create Ejercicio 5.py

---
+++ Ejercicios Iniciales/Ejercicio 5.py
@@ -0,0 +1,7 @@
+#Programa que pida cinco palabras y muestre una frase con las cinco. Modifica el código para que entre palabra y palabra haya una coma.
+Palabra_1= input("Introduce la primera palabra: ")
+Palabra_2= input("Introduce la segunda palabra: ")
+Palabra_3= input("Introduce la tercera palabra: ")
+Palabra_4= input("Introduce la cuarta palabra: ")
+Palabra_5= input("Introduce la quinta palabra: ")
+print("Las cinco palabras son: ",Palabra_1,",",Palabra_2,",",Palabra_3,",",Palabra_4,",",Palabra_5)

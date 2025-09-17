566bfdae01ff47ed47148f0b14be9ae6184ce1da
Pau-Rodriguez-Gago <pau.rodriguez@campus.xaloc.org>
2025-09-17 09:56:15 +0200

    Create Ejercicio 4.py

---
+++ Ejercicios Iniciales/Ejercicio 4.py
@@ -0,0 +1,5 @@
+#4.- A partir del código anterior, realiza una versión para números con decimales
+var1= float(input("Introduce un número decimal: "))
+var2= float(input("Introduce otro número decimal: "))
+resultado= var1 + var2
+print("La suma de los dos números decimales es: ",resultado)

6293e4b3165a03d9db20ec4122aa203a4020237f
Pau-Rodriguez-Gago <pau.rodriguez@campus.xaloc.org>
2025-09-17 09:56:23 +0200

    Create Ejercicio 6.py

---
+++ Ejercicios Iniciales/Ejercicio 6.py
@@ -0,0 +1,7 @@
+#6.-A partir del programa 5. Haz que se muestre por pantalla también la frase en el orden inverso en que se han introducido las palabras.
+Palabra_1= input("Introduce la primera palabra: ")
+Palabra_2= input("Introduce la segunda palabra: ")
+Palabra_3= input("Introduce la tercera palabra: ")
+Palabra_4= input("Introduce la cuarta palabra: ")
+Palabra_5= input("Introduce la quinta palabra: ")
+print("Las cinco palabras en orden invertido son: ",Palabra_5,",",Palabra_4,",",Palabra_3,",",Palabra_2,",",Palabra_1)

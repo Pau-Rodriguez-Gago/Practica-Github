3e814db414529ec4c7b3582e0978b8987777184d
Pau-Rodriguez-Gago <pau.rodriguez@campus.xaloc.org>
2025-09-17 09:56:09 +0200

    Create Ejercicio 2.py

---
+++ Ejercicios Iniciales/Ejercicio 2.py
@@ -0,0 +1,7 @@
+# Programa que pide tres variables y las muestra en orden: entero, texto, decimal
+var1= int(input("Introduce un número entero: "))
+var2= input("Introduce un texto: ")
+var3 = float(input("Introduce un número decimal: "))
+print("el número entero introducido es: ",var1)
+print("el texto introducido es: ",var2)
+print("el número decimal introducido es: ",var3)

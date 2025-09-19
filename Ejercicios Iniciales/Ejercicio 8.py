0868cdb6c28a01f66ec3db88193be1b9c05647fc
Pau-Rodriguez-Gago <pau.rodriguez@campus.xaloc.org>
2025-09-19 16:04:12 +0200

    Create Ejercicio 8.py

---
+++ Ejercicios Iniciales/Ejercicio 8.py
@@ -0,0 +1,5 @@
+#8.-Programa que pida un número de horas y muestre por pantalla los minutos y segundos
+horas= int(input("Introduce un número de horas: "))
+minutos= horas * 60
+segundos= minutos * 60
+print("Son",horas,"horas o",minutos," minutos o ",segundos," segundos.")

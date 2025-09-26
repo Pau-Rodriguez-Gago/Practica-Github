5bf9d6b9ffcd63ed9cc4032bb369589e32b6eb50
Pau-Rodriguez-Gago <pau.rodriguez@campus.xaloc.org>
2025-09-26 15:28:13 +0200

    Update Apuntes para el examen.py

---
+++ Apuntes primera clase/Apuntes para el examen.py
@@ -0,0 +1,30 @@
+#Operaciones básicas con dos números:
+#para mostrar un número con dos decimales se usa format y (la variable y, ".2f")
+num1= int(input("Introduce el primer número: "))
+num2= int(input("Introduce el segundo número: "))
+#El int es para que lo tome como número entero, y el float es para que lo tome como número decimal, el input es para que el usuario introduzca el valor
+#en python los nombres de las variables no pueden llevar espacios
+#las operaciones son: + - * / ** // %
+#** es potencia
+#// es cociente
+#% es resto
+suma= num1 + num2
+resta= num1 - num2
+multiplicacion= num1 * num2
+division= num1 / num2
+division_2decimales= format(division, '.2f')
+exponente= num1 ** num2
+cociente= num1 // num2
+resto= num1 % num2
+#para redondear un número se usa round(variable, número de decimales)
+resto_2decimales= round(resto, 2)
+#El if es para hacer una condición, si se cumple lo que hay después del if se ejecuta lo que hay debajo, si no se cumple no se ejecuta
+if num1 > num2:
+ print("El primer número es mayor que el segundo")
+ #El else es para hacer una condición contraria al if, si no se cumple lo que hay después del if se ejecuta lo que hay debajo del else
+else:
+ print("El segundo número es mayor que el primero")
+ #Importar el math nos perimite usar la función math.pi para el valor de π y la raíz cuadrada con math.sqrt()
+import math
+raiz_cuadrada_num1= math.sqrt(num1)
+raiz_cuadrada_num2= math.sqrt(num2)

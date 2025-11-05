24a0080edbb34079ccca1d3170f5d2af90583aa9
Pau-Rodriguez-Gago <pau.rodriguez@campus.xaloc.org>
2025-11-05 09:54:58 +0100

    Create Ejercicio 35.py

---
+++ Ejercicios con bucles/Ejercicio 35.py
@@ -0,0 +1,5 @@
+#35. Programa que al introducir un número por teclado permita mostrar ese número de veces tu 
+#nombre
+número_veces=input("Introduce un número de veces que quieres que salga tu nombre: ")
+for i in número_veces:
+    print("Pau Rodríguez Gago")

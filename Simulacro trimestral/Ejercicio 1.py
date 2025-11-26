d881daf050f33ccba1ff46b8c27ce17bdd59fa78
Pau-Rodriguez-Gago <pau.rodriguez@campus.xaloc.org>
2025-11-26 09:56:38 +0100

    Update Ejercicio 1.py

--- Simulacro trimestral/Ejercicio 1.py
+++ Simulacro trimestral/Ejercicio 1.py
@@ -1,3 +1,4 @@
+concatenar=""
 inicio=0
 fin=0
 incremento=0
@@ -5,8 +6,9 @@ incio=int(input("Introduce el inicio: "))
 fin=int(input("Introduce el fin: "))
 incremento=int(input("Introduce un incremento: "))
 for i in range(incio,fin,incremento):
-    if not(i/4==i//4) and not(i/6==i//6):
-        print(i,",")
-    elif i/6==i//6:
-        print("*",i,"*")
-    
+    if not i%4==0:
+        if i%6==0:
+            concatenar=concatenar+"*" + str(i)+ "*" ","
+        else:
+            concatenar=concatenar+ str(i) + ","
+print(concatenar[:-1])

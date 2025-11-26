7bd558efb61e159a5c08ba9206e7ad3f2a52c71e
Pau-Rodriguez-Gago <pau.rodriguez@campus.xaloc.org>
2025-11-26 09:03:49 +0100

    Create Ejercicio 2.py

---
+++ Simulacro trimestral/Ejercicio 2.py
@@ -0,0 +1,15 @@
+numero=0
+suma_numeros_positivos=0
+suma_numeros_negativo=0
+suma_numeros_mayor_a_cien=0
+for x in range(0,7):
+    numero=int(input("Introduce uno de los 7 números: "))
+    if numero<0:
+        suma_numeros_negativo=suma_numeros_negativo+numero
+    elif numero>0:
+        suma_numeros_positivos=suma_numeros_positivos+numero
+        if numero>100:
+            suma_numeros_mayor_a_cien=suma_numeros_mayor_a_cien+1
+print("Suma positivos: ",suma_numeros_positivos)
+print("Suma negativos: ",suma_numeros_negativo)
+print("Mayores de 100: ", suma_numeros_mayor_a_cien)

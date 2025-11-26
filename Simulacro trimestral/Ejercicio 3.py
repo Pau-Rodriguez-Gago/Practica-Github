5ff20194e77d4392a278de07e25627060338b15e
Pau-Rodriguez-Gago <pau.rodriguez@campus.xaloc.org>
2025-11-26 09:56:47 +0100

    Update Ejercicio 3.py

--- Simulacro trimestral/Ejercicio 3.py
+++ Simulacro trimestral/Ejercicio 3.py
@@ -1,13 +1,15 @@
 numero_de_cifras=0
 numero_introducido=0
+longitud_numero=0
 producto=1
 total_de_pares=0
 numero_de_cifras=int(input("Introduce cuantas cifras quieres que tenga tu número: "))
-numero_introducido=str((input("Introduce el número: ")))
-if len(numero_introducido)==numero_de_cifras:
-    for x in range(0,numero_de_cifras):
-        producto=producto*numero_introducido[x]
-        if numero_introducido[x]/2==numero_introducido//2:
+numero_introducido=int((input("Introduce el número: ")))
+longitud_numero=len(str(numero_introducido))
+if longitud_numero==numero_de_cifras:
+    for x in str(numero_introducido):
+        producto=producto*int(x)
+        if int(x)%2==0:
             total_de_pares=total_de_pares+1
     print("Producto de cifras: ",producto)
     print("Cifras pares: ", total_de_pares)

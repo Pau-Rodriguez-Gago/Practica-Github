ddc7a330cd3444e2a450a4a7c5443a86a6780ba4
Pau-Rodriguez-Gago <pau.rodriguez@campus.xaloc.org>
2025-12-02 10:45:32 +0100

    Create Ejercicio 57.py

---
+++ Ejercicios while/Ejercicio 57.py
@@ -0,0 +1,11 @@
+#57. Realiza un programa que permita adivinar un número comprendido entre 1 y 5. El programa 
+#debe controlar si el usuario introduce un número no comprendido entre 1 y 5
+import random
+numero=random.randint(1,5)
+numero_intro=0
+while not(numero==numero_intro):
+    numero_intro=int(input("Introduc el número que crees que es: "))
+    if numero_intro==numero:
+     print("Número acertado")
+    else:
+       print("Número no acertado")

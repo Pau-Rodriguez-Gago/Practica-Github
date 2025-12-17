af0de86acfaa90ccfa039cfb93dd06bab82dc47e
Pau-Rodriguez-Gago <pau.rodriguez@campus.xaloc.org>
2025-12-17 09:53:24 +0100

    gugugaga

--- Proyecto Password/Proyecto Password V2.py
+++ Proyecto Password/Proyecto Password V2.py
@@ -3,7 +3,7 @@ print("1.Debe contener un número entre el 0 y el 3")
 print("2.Debe contener un número entre el 4 y el 6")
 print("3.Debe contener un número entre el 7 y el 9")
 print("4.Debe contener como mínimo dos letras mayúsculas y una minúscula.")
-print("5.Debe contener un mínimo de 2 símbolos a excepción de una coma, un punto o un espacio.")
+print("5.Debe contener un mínimo de 2 símbolos de entre estos /,!,¿,?,¡,+,-,@")
 print("6.No debe contener dos letras, números o símbolos, seguidos.")
 print("7.Debe contener un mínimo de 12 caracteres")
 password="inicializada"
@@ -29,7 +29,7 @@ while otro_password=="s" and not (passwords_restantes==0):
     if len(password)<12:
         print("Error, su contraseña tiene un total de",len(password),"caracteres y no llega a los 12 requeridos.")
     for x in password:
-        if x.isnumeric():
+        if x.isdigit():
             numero_seguido=numero_seguido+1
             letra_seguida=0
             simbolo_seguido=0
@@ -39,18 +39,21 @@ while otro_password=="s" and not (passwords_restantes==0):
                 contador_cuatro_seis=contador_cuatro_seis+1
             if int(x)>=7 and int(x)<=9:
                 contador_siete_nueve=contador_siete_nueve+1
-        if x.isalpha():
+        if x.isupper():
             letra_seguida=letra_seguida+1
             numero_seguido=0
             simbolo_seguido=0 
-            if str(x).isupper():
-                contador_letras_mayus=contador_letras_mayus+1
-            if str(x).islower():
+            contador_letras_mayus=contador_letras_mayus+1
+        if x.islower():
                 contador_letras_minus=contador_letras_minus+1
-        if not(x.isalnum()) and not(x==" " or x=="." or x==","):
+                letra_seguida=letra_seguida+1
+                numero_seguido=0
+                simbolo_seguido=0 
+        if x in ["/","!","¿","?","¡","+","-","@"]:
             simbolo_seguido=simbolo_seguido+1
             letra_seguida=0
             numero_seguido=0
+            contador_simbolos=contador_simbolos+1
         if numero_seguido==2:
             veces_que_hay_numero_seguido=veces_que_hay_numero_seguido+1
             numero_seguido=0
@@ -76,7 +79,7 @@ while otro_password=="s" and not (passwords_restantes==0):
         print("La contraseña debe incluir al menos una letra minúscula.")
         password_perfecto="no"
     if not(contador_simbolos>=2):
-        print("La contraseña debe incluir un mínimo de dos símbolos que no sean ni una coma ni un punto ni un espacio")
+        print("La contraseña debe incluir un mínimo de dos símbolos de entre estos /,!,¿,?,¡,+,-,@")
         password_perfecto="no"
     if not(veces_que_hay_letra_seguida==0):
         print("La contrseña no debe tener dos letras seguidas, usted ha introducido dos letras seguidas en:",veces_que_hay_letra_seguida,"ocasiones.")
@@ -101,6 +104,11 @@ while otro_password=="s" and not (passwords_restantes==0):
     letra_seguida=0
     simbolo_seguido=0
 if passwords_restantes==0 and otro_password=="s":
-    print("lo siento, pero aunque quieras seguir, te has quedado sin intentos.")
+    print("Lo siento, pero aunque quieras seguir, te has quedado sin intentos.")
 print("Has introducido",passwords_correctos,"contraseñas correctas.")
 print("Y has introducido",passwords_incorrectos,"contraseñas incorrectas.")
+#Pruebas:
+#Entrada:                                         Salida:
+#1A5B8c/7B@c9K+                                   Tu contraseña es correcta!
+#3l6P7h@G0+d5                                     Tu contraseña es correcta!
+

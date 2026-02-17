26791baad7383aeb0b4c6f11dbae488499b52512
Pau-Rodriguez-Gago <pau.rodriguez@campus.xaloc.org>
2026-02-17 10:28:52 +0100

    h

--- Ejercicios Jutge/Integer division and remainder of two natural numbers.py
+++ Ejercicios Jutge/Integer division and remainder of two natural numbers.py
@@ -1,4 +1,6 @@
-ab=(input("").split())
-a=(ab[0])
-b=(ab[1])
-print(int(a)//int(b),int(a)%int(b))
+numeros=(input("").split())
+num1=(numeros[0])
+num2=(numeros[1])
+print(int(num1)//int(num2),int(num1)%int(num2))
+#Básicamente, separo los numero en dos variables con el split y les doy el valor de su posición
+#en la lista. Luego, hago la división entera con // y el resto con % y los imprimo.

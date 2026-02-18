142ccaf70d0d9b1403b733fdab021e1f83badc9d
Pau-Rodriguez-Gago <pau.rodriguez@campus.xaloc.org>
2026-02-18 09:52:58 +0100

    Create Ahorcado.py

---
+++ Ahorcado.py
@@ -0,0 +1,19 @@
+lista_palabrasecreta=["gigante","arqueras","mago","cementerio","ballesta","mortero","golem","mosquetera","tronco","esqueleto"]
+lista_partida=[]
+lista_ahorcado=[]
+import random
+palabra_escogida=random.choice(lista_palabrasecreta)
+for i in palabra_escogida:
+    lista_partida.append(str("_"))
+print(*lista_partida)
+letra_escogida=input("")
+lista_partida=[]
+if letra_escogida in palabra_escogida:
+    for i in palabra_escogida:
+        if letra_escogida==i:
+            lista_partida.append(letra_escogida)
+        else:
+            lista_partida.append(str("_"))
+else:
+    print("La letra no está en la palabra")
+print(*lista_partida)
